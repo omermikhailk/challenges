22d740258e26f3d8eeda36a19a0bec7e3c2e773f
Omer Mikhail Khan <106922795+omermikhailk@users.noreply.github.com>
2022-08-08 18:42:16 +0000

    Add docstrings

--- project_euler/problem007.py
+++ project_euler/problem007.py
@@ -2,6 +2,14 @@ from euler import sieve_of_eratosthenes
 
 
 def get_prime(n: int) -> int:
+    """Generates the `n`-th prime and returns it.
+
+    Args:
+        n (int): The `n`-th prime that we want.
+
+    Returns:
+        int: The value of the `n`-th prime.
+    """
     for index, prime in enumerate(sieve_of_eratosthenes(), 1):
         if index == n:
             return prime
